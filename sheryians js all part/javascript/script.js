42ba15bf91e66b02187e3de6ae56aa0733cb8240
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-10-31 01:30:30 +0545

    js practice today primitive data type

---
+++ sheryians js all part/javascript/script.js
@@ -0,0 +1,32 @@
+
+// {
+//     var a = 1;
+//     let b = 21;
+// }
+
+// console.log(a);
+// console.log(b);
+
+
+
+// const person = {name: "Laxman"};
+// person.name = "Lakxh"
+// // person = {name: Saru}
+
+// console.log(person)
+
+
+// const a = 9007199254740991n;   Number.max
+
+// let obj = {
+//     uid: 288,
+//     name: "Lakxh",
+//     address: "Kathmandu",
+//     email: "ghimirelaxman2155@gmail.com",
+//     phone: 987663636363,
+// };
+
+// let uniqueId = Symbol("uid");
+// obj[uniqueId] = "07";
+
+// console.log(obj)
