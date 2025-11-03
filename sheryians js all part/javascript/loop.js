19371fd97a3dc2a6c43b0c52daf34d3d72806585
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-11-03 21:30:35 +0545

    count

--- sheryians js all part/javascript/loop.js
+++ sheryians js all part/javascript/loop.js
@@ -10,16 +10,31 @@
 // console.log(i++)
 
 
-prompt("Scissors, paper, rock")
+// prompt("Scissors, paper, rock")
 
-function rps(user, computer){
+// function rps(user, computer){
 
-if(user === computer) return "draw";
+// if(user === computer) return "draw";
 
-if(user === "rock" && computer === "scissor") return "user won";
-if(user === "paper" && computer === "rock") return "user won";
-if(user === "scissor" && computer === "paper") return "user won";
+// if(user === "rock" && computer === "scissor") return "user won";
+// if(user === "paper" && computer === "rock") return "user won";
+// if(user === "scissor" && computer === "paper") return "user won";
 
-else return "computer won";
+// else return "computer won";
+// }
+// console.log(rps("", "paper"));
+
+
+for(let i=1; i<10; i++){
+    console.log(`The multiplication of 5 x ${i} = ${5*i}`)
 }
-console.log(rps("", "paper"));
+
+
+let count = 0;
+
+for (let i=1; i<=15; i++){
+    if(i>8){
+        count++
+    }
+}
+console.log(`The count is ${count}`)
