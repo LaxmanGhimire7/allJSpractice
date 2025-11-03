8dfd21e536989dedc7b908d5463960bafe0e2120
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-11-03 23:23:18 +0545

    js

--- sheryians js all part/javascript/loop.js
+++ sheryians js all part/javascript/loop.js
@@ -1,5 +1,3 @@
-
-
 // for(let i=10; i>0; i--){
 //  console.log(i)
 // }
@@ -8,7 +6,6 @@
 
 // console.log(++i);
 // console.log(i++)
-
 
 // prompt("Scissors, paper, rock")
 
@@ -24,17 +21,36 @@
 // }
 // console.log(rps("", "paper"));
 
+// for(let i=1; i<10; i++){
+//     console.log(`The multiplication of 5 x ${i} = ${5*i}`)
+// }
 
-for(let i=1; i<10; i++){
-    console.log(`The multiplication of 5 x ${i} = ${5*i}`)
+// let count = 0;
+
+// for (let i=1; i<=15; i++){
+//     if(i>8){
+//         count++
+//     }
+// }
+// console.log(`The count is ${count}`);
+
+// Ask user’s age and check if eligible to vote
+// If age >= 18 → “Eligible”, else → “Not eligible”
+
+let age = prompt("What's your age?");
+
+if (age === null) {
+  console.log("You cancelled!");
+} else {
+  if (age.trim() === "") {
+    console.log("Enter anything dont keep null value");
+  } else {
+    age = Number(age);
+
+    if (age < 0) {
+      console.log("age cant be in negative value");
+    }else if (age >= 18) {
+      console.log("You can vote");
+    } else console.log("Cant vote");
+  }
 }
-
-
-let count = 0;
-
-for (let i=1; i<=15; i++){
-    if(i>8){
-        count++
-    }
-}
-console.log(`The count is ${count}`)
