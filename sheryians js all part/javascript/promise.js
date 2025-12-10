6ede24e05a7675283e250b04e96f0914b70f0a60
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-10 07:25:51 +0545

    fetched api data

--- sheryians js all part/javascript/promise.js
+++ sheryians js all part/javascript/promise.js
@@ -1,13 +1,18 @@
-const prm = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject();
-  }, 3000);
-});
+// const prm = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject();
+//   }, 3000);
+// });
 
-prm
-  .then(() => {
-    console.log("resolved");
-  })
-  .catch(() => {
-    console.log("rejected");
-  });
+// prm
+//   .then(() => {
+//     console.log("resolved");
+//   })
+//   .catch(() => {
+//     console.log("rejected");
+//   });
+
+// fetch((`https://dummyjson.com/products`)=>{})
+let data = fetch(`https://dummyjson.com/products`)
+  .then((raw) => raw.json())
+  .then((fresh) => console.log(fresh.products[4].title));
