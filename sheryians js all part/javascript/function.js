9abe718884103db113fe8b5f6adb6ad7ed2c512b
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-16 20:23:55 +0545

    function complete

--- sheryians js all part/javascript/function.js
+++ sheryians js all part/javascript/function.js
@@ -48,8 +48,6 @@
 // hihi()
 // hihi()
 
-
-
 // Lexical Scoping
 // function abcd() {
 //   let a = 12;
@@ -64,4 +62,98 @@
 //   defg()
 // }
 // abcd()
+
+// //Closure
+// function hii(){
+// let a = 12;
+
+// function next(){
+//     console.log(a)
+// }
+//  return next
+// }
+
+// let fn = hii();
+// fn();
+
+// IIFE
+// (function(){
+//     console.log("Hii")
+// })()
+
+//Hoisting in function
+// abcd();
+
+// function abcd(){
+//     console.log("hiii")
+// }
+
+// function multiply(a,b){
+//     return a*b
+// }
+
+// let multiply = (a,b)=>{
+// return a *b
+// }
+
+// function getScore(...val){
+//     let sum = 0;
+//     val.forEach((num)=>{
+//         sum = sum + num;
+//     })
+//     return sum;
+// }
+
+// console.log(getScore(10,20,30,40,50))
+
+// function abcd(val){
+//     val()
+// }
+
+// abcd(function(){
+//     console.log("Laxman Ghimire")
+// })
+
+// Closure practice
+// function outer(){
+// let count = 0
+//     return function(){
+//    count++
+//    console.log(count)
+//     }
+// }
+
+// let counter = outer();
+// counter()
+// counter()
+
+//convert this basic function in IIFE
+
+// function init(){
+//     console.log("Initialized")
+// }
+
+// (function(){
+//     console.log("Initialized")
+// })();
+
+//write a BMI calculator
+
+// function bmi(weight, height){
+//     return weight / (height*height)
+// }
+// console.log(bmi(60, 1.65).toFixed(2))
+
+// Discounter
+
+// function discountPercent(discount) {
+//   return function (price) {
+//    return price - price * (discount / 100);
+//   };
+// }
+
+// let discounter = discountPercent(10);
+// console.log(discounter(2000));
+
+
 
