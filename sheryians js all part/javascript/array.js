1bd0dab26a72a1a6deb60b3ccae7377c2378ca28
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-17 20:41:10 +0545

    array done

--- sheryians js all part/javascript/array.js
+++ sheryians js all part/javascript/array.js
@@ -1,2 +1,126 @@
-let marks = [1,2,3,4,5]
-console.log(marks.shift())
+// let marks = [1,2,3,4,5]
+
+
+// new way to create array
+
+// let srr = new Array(1,2,3,4,5);
+// console.log(srr)
+
+//Modfying array
+// let arr = [1,2,3,4]
+// arr[2] = 12;
+// console.log(arr);
+
+
+// let fruits = ['banana', 'apple', 'orange', 'pineapple']
+
+// // fruits.push("Lakxh")
+// // fruits.pop()
+// console.log(fruits)
+
+
+// let arr = [1,2,3,4,5,6]
+
+// // arr.shift()
+// // arr.unshift(0)
+// // arr.splice(1,3)  1 is from which index hatauna suru garne and 3 is kati ota so output vaneko 1,5,6
+// // let newArr = arr.slice(2,3) output is 3
+
+// console.log(arr)
+// console.log(newArr)
+
+
+
+// let fruits = ['banana', 'apple', 'peach', 'kiwi']
+// fruits.reverse();
+// console.log(fruits)
+
+
+// let num = [26,67,11,89,99,55]
+// let newNum = num.sort((a,b)=>{
+// return b-a;
+// })
+// console.log(newNum)
+
+
+
+
+// let num = [1,2,3,4,5,6,7,8]
+
+// num.forEach((val)=>{
+//     console.log(val+3)
+// })
+
+
+// let nums = [1,2,3,4,5,6,7,8]
+
+// let neW = nums.map((num)=>{
+//  return num+3;
+// })
+// console.log(neW)
+// console.log(nums)
+
+// let nums = [1,2,3,4,5,6,7,8]
+
+// let newNum = nums.filter((val)=>{
+//     if(val > 5) return true;
+// })
+// console.log(newNum)
+
+
+// let arr = [1,2,3]
+// let newarr = arr.reduce((acc, val)=>{
+//   return acc + val;
+// })
+// console.log(newarr)
+
+
+
+// let arr = [1,2,3,4,5,6];
+
+// let [a,b,...c] = arr;
+ 
+// a = 20;
+
+// console.log(a,b,...c)
+// console.log(arr)
+
+
+
+// q1
+// let fruits = ['apple', 'banana', 'mango']
+// console.log(fruits[1])
+
+
+// q2 add "mango" at the end and "pineapple" at the front of this array
+
+// let fruits = ["Apple", "Banana"]
+
+// fruits.push("Mango")
+// fruits.unshift("Pineapple")
+
+
+// q3. replace "banana" with kiwi 
+
+// let fruits = ["Apple", "Banana"]
+
+// fruits[1] = "kiwi"
+// console.log(fruits)
+
+//q.4 remove the last item from this array using a method
+
+// let arr = [1,2,3,4,5]
+// arr.pop()
+// console.log(arr)
+
+
+// q5. Insert "Red" and "Blue" at the 1st index
+
+// let colors = ["White", "Brown"]
+// colors.splice(1,0,"Red", "Blue")
+
+// q6. Extract only middle 3 from this element
+// let numbers = [1,2,3,4,5,6]
+// let newa = numbers.slice(1,4)
+
+// console.log(newa)
