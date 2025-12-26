d309697f7807f7737b839ba37314b3cb518e7317
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-26 17:18:17 +0545

    pnjects

--- sheryians js all part/javascript/object.js
+++ sheryians js all part/javascript/object.js
@@ -5,7 +5,44 @@ let obj = {
     duration: "3 months"
 }
 
-for(let key in obj){
-    console.log(key)
+// let obj2 = {...obj};
+
+// object.assign()
+// let obj2 = Object.assign({},obj)
+// console.log(obj2)       
+
+
+// console.log(obj.name, " ,", obj.course, obj.duration)
+
+let objs = {
+    name:"Laxman Ghimire",
+    age: 22,
+    email: "ghimirelaxman2155@gmail.com",
+    details:{
+        skills: "MERN Stack",
+        docker: "Hi"
+    }
 }
+
+// let newObj = {...objs}
+// newObj.details.skills = "Django"
+
+// console.log(newObj)
+// console.log(objs)
+
+let obj2 = JSON.parse(JSON.stringify(objs))
+obj2.details.skills = "LKaxkn"
+console.log(obj2)
+console.log(objs)
+
+
+
+// console.log(objs.details.docker)
+
+// var h1 = document.querySelector('h1');
+// h1.addEventListener('click',()=>{
+//     h1.innerHTML = 'Hola'
+// })
+
+
 
